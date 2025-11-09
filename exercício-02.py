4493a8448035e5f1f516eeb88e6424b2c633ec18
arken012 <arken123br@gmail.com>
2025-11-09 15:14:55 -0300

    feat: textexercício-02

---
+++ exercício-02.py
@@ -0,0 +1,10 @@
+
+
+numero_str = input("Digite um número para ver a tabuada: ")
+numero = int(numero_str)
+
+    print(f"\n--- Tabuada do {numero} ---")
+
+    for i in range(1, 11):
+        resultado = numero * i
+        print(f"{numero} x {i} = {resultado}")
